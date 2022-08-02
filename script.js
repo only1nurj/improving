fd0094ef60d127b348558b6a02834c0fe213c719
Rita <jritaun@gmail.com>
2022-08-02 20:53:21 +0100

    README.md

---
+++ script.js
@@ -0,0 +1,60 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) =>  {
+    event.target.style.background = "blue";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("image2");
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "lemon";
+});
+
+const change3 = document.getElementById("image3");
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+});
+
+const change4 = document.getElementById("image4");
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("image5");
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "purple";
+    event.target.style.color = "pink";
+});
+
+const change6 = document.getElementById("image6");
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "orange";
+    event.target.style.color = "violet";
+});
+
+const change7 = document.getElementById("image7");
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "gray";
+});
+
+const change8 = document.getElementById("image8");
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "pink";
+    event.target.style.color = "red";
+});
+
+const change9 = document.getElementById("image9");
+change9.addEventListener("click", (event) => {
+    event.target.style.background = "violet";
+    event.target.style.color = "purple";
+});
+
+const change10 = document.getElementById("image10");
+change10.addEventListener("click", (event) => {
+    event.target.style.background = "gray";
+    event.target.style.color = "blue";
+});
